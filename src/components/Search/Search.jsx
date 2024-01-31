9ece6604cebd5524a20178ca90036ad433b7d14d
vipinrao009 <vipinrao2018@gmail.com>
2024-02-01 02:06:23 +0530

    fix search button in center

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,4 +1,3 @@
-import PokemonList from "../PokemonList/PokemonList"
 import "./Search.css"
 function Search(){
     return(
@@ -8,7 +7,6 @@ function Search(){
           type="text"
           placeholder="Enter Pokemon...."
         />
-        <PokemonList/>
         </div>
     )
 }
